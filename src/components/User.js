190cf32a148e6d4909b9fcd3f45abc41e4ad13ce
OlegAleksashov <fairp88@tut.by>
2023-09-22 18:35:09 +0300

    fixing in User.js

--- src/components/User.js
+++ src/components/User.js
@@ -9,7 +9,7 @@ function User(props) {
         <IoHammerSharp className="edit-icon"/>
         <h3>{props.friend.firstname} {props.friend.secondname}</h3>
         <p>{props.friend.bio}</p>
-        <b>{props.friend.isHappy ? 'Happy!' : 'Sad...' }</b> 
+        <b>{props.friend.isHappy ? 'Happy!' : 'Sad...'}</b> 
     </div>
     );
 }
